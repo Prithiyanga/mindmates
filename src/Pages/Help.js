82e8ab22cac42f2899fa9c4114ce453a3c194cb0
Worldis-mine <148679653+Worldis-mine@users.noreply.github.com>
2024-09-05 21:30:32 +0530

    Add files via upload
    Help and Insight updation.

--- src/Pages/Help.js
+++ src/Pages/Help.js
@@ -1,11 +1,232 @@
-import React from "react";
+/* import React from "react";
 import Header from "../Header";
+import "../Styles/Help.css"
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+import Logo from "../Media/Logo.png";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
+
+
+
 
 const Help = () => {
+const cip=document.querySelector(
+  '.cip');
+cip.addEventListener(onclick,(event=>{
+  event.target.placeholder="";
+}))
+
+
+
+
+
+
+
   return (
-    <div>
+    <div className="help-full-container">
       <Header />
-      <div>Help</div>
+      <div className="help-container">
+        <div className="help-header">
+          <h1 id="title">Help desk support</h1>
+          <div className="button-section"> 
+            <button className=" help-button"> learn more<IoIosArrowForward /> </button>
+            <button className=" help-button"><IoIosArrowBack />for Enqiury</button>
+          </div>
+        </div>
+        <div className="footer">
+        <div className="logo-div">
+          <img src={Logo} alt="Mind Mate Logo" className="header-logo-help-page" />
+          <h1 id="logo-name">Mind Mate</h1>
+        </div>
+        <div className="contacts">
+          <div className="cinputs"> 
+              <input className="cip" placeholder="NAME" />
+              <input className="cip" placeholder="E-MAIL" />
+              <input className="cip"  placeholder="PHONE"/>
+          </div>
+          <div className="more-contacts">
+          <textarea className="contacts-textarea" placeholder="MESSAGE">
+
+          </textarea>
+           <button id="message-btn">SUBMIT</button>
+
+
+          </div>
+          <div className="quick-links">
+            <h2 id="ql">Quick Links
+
+            </h2>
+            <ul className="links">
+              <li>
+                Home
+              </li>
+              <li>
+                About
+                
+              </li>
+              <li>
+                Insight
+                
+              </li>
+              <li>
+                learn
+                
+              </li>
+ 
+ 
+
+            </ul>
+            <div className="contact-link">
+            <h2 className="contact-title">contact</h2>
+            <p><FaPhoneAlt className="icon" />
+            <span className="content">91+ 8148102652</span>
+            </p>
+            <p><MdEmail className="icon" />
+            <span className="content">mitbots01@gmail.com</span>
+            </p>
+
+            </div>
+            
+           
+
+          </div>
+          
+        </div>
+        <div>
+
+        </div>
+
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Help;
+ */
+import React, { useEffect, useRef } from "react";
+import Header from "../Header";
+import "../Styles/Help.css";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import Logo from "../Media/Logo.png";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+const Help = () => {
+  const cipRefs = useRef([]);
+
+  useEffect(() => {
+    const elements = cipRefs.current;
+
+    if (elements.length) {
+    
+      elements.forEach((element) => {
+        if (element) {
+          element.addEventListener('click', (event) => {
+            event.target.placeholder = "";
+          });
+        }
+      });
+
+     
+      return () => {
+        elements.forEach((element) => {
+          if (element) {
+            element.removeEventListener('click', (event) => {
+              event.target.placeholder = "";
+            });
+          }
+        });
+      };
+    }
+  }, []);
+
+  return (
+    <div className="help-full-container">
+      <Header />
+      <div className="help-container">
+        <div className="help-header">
+          <h1 id="title">Help desk support</h1>
+          <div className="button-section">
+            <button className="help-button">
+              learn more <IoIosArrowForward />
+            </button>
+            <button className="help-button">
+              <IoIosArrowBack />
+              for Enquiry
+            </button>
+          </div>
+        </div>
+        <div className="footer">
+          <div className="logo-div">
+            <img
+              src={Logo}
+              alt="Mind Mate Logo"
+              className="header-logo-help-page"
+            />
+            <h1 id="logo-name">Mind Mate</h1>
+          </div>
+          <div className="contacts">
+            <div className="cinputs">
+              {}
+              <input
+                className="cip"
+                placeholder="NAME"
+                ref={(el) => (cipRefs.current[0] = el)}
+              />
+              <input
+                className="cip"
+                placeholder="E-MAIL"
+                ref={(el) => (cipRefs.current[1] = el)}
+              />
+              <input
+                className="cip"
+                placeholder="PHONE"
+                ref={(el) => (cipRefs.current[2] = el)}
+              />
+            </div>
+            <div className="more-contacts">
+              <textarea
+                className="contacts-textarea"
+                placeholder="MESSAGE"
+                ref={(el) => (cipRefs.current[3] = el)}
+              />
+              <button id="message-btn">SUBMIT</button>
+            </div>
+            <div className="quick-links">
+              <h2 id="ql">Quick Links</h2>
+              {/* <ul className="links">
+                <li>Home</li>
+                <li>About</li>
+                <li>Insight</li>
+                <li>Learn</li>
+              </ul> */}
+              <div className="links">
+              <Link to="/" className="header-option1"><p>Home</p></Link>
+              <Link to="/AboutMindMate" className="header-option1"><p>About</p></Link>
+              <Link to="/InsightMindMateReportInput" className="header-option1"><p>Insight</p></Link>
+              <Link to="/MindMateCourseOfferings" className="header-option1"> <p>Learn</p></Link>
+              </div>
+              <div className="contact-link">
+                <h2 className="contact-title">Contact</h2>
+                <p>
+                  <FaPhoneAlt className="icon" />
+                  <span className="content">91+ 8148102652</span>
+                </p>
+                <p>
+                  <MdEmail className="icon" />
+                  <span className="content">mitbots01@gmail.com</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Insight.js
+++ src/Pages/Insight.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../Header";
 import Doctor from "../Media/doctor.png";
 import Parent from "../Media/parent.png";
 import Teacher from "../Media/teacher.png";
 import "../Styles/Insight.css";
+import { FiPlus } from "react-icons/fi";
+import { PiFilePdfThin } from "react-icons/pi";
+import { MdCancel } from "react-icons/md";
 
 const Insight = () => {
+  const [fileUrl, setFileUrl] = useState(null);
+  const [bg,setBg]=useState(true)
+
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setFileUrl(url);
+      console.log(file.name)
+    }
+    
+  };
+
   return (
     <div>
       <Header />
-      <div className="insight-container">
+      <div className="insight-container" style={{backgroundColor:bg?"none ":"white"}}>
         <div className="in-users" id="user-form">
           <div className="user">
             <img src={Teacher} className="user-icon" alt="Teacher" />
@@ -20,32 +37,35 @@ const Insight = () => {
               onClick={() => {
                 document.getElementById("user-form").style.display = "none";
                 document.getElementById("rep-form").style.display = "flex";
+                setBg(false)
               }}
             >
               Login
             </button>
           </div>
           <div className="user">
-            <img src={Doctor} className="user-icon" alt="Teacher" />
+            <img src={Doctor} className="user-icon" alt="Doctor" />
             <div className="user-name">Doctor</div>
             <button
               className="login"
               onClick={() => {
                 document.getElementById("user-form").style.display = "none";
                 document.getElementById("rep-form").style.display = "flex";
+
               }}
             >
               Login
             </button>
           </div>
           <div className="user">
-            <img src={Parent} className="user-icon" alt="Teacher" />
+            <img src={Parent} className="user-icon" alt="Parent" />
             <div className="user-name">Parent</div>
             <button
               className="login"
               onClick={() => {
                 document.getElementById("user-form").style.display = "none";
                 document.getElementById("rep-form").style.display = "flex";
+
               }}
             >
               Login
@@ -53,49 +73,64 @@ const Insight = () => {
           </div>
         </div>
         <div className="in-box" id="rep-form">
-          <div className="in-field">
-            <label className="inq">What is the IQ?</label>
-            <input className="inp" type="text"></input>
+        {fileUrl ? <MdCancel  className="cross" onClick={()=>{
+          setFileUrl(null)
+
+        }}/> : " "}
+          <div className="dwnld">
+          
+          <PiFilePdfThin className="tbfile"/>
+          <a href="/Template.pdf" className="anchor"  download="Template.pdf">
+            <button id="pdf">Download Template</button>
+          </a>
           </div>
-          <div className="in-field">
-            <label className="inq">What is the Cognitive Referenced Age?</label>
-            <input className="inp" type="number"></input>
+          <div className="add">
+           
+            {fileUrl ? (
+             
+             <> <iframe
+             src={fileUrl}
+             title="Selected File"
+             className="file-preview"
+             style={{ width: "100%", height: "500px", border: "none" }}
+           ></iframe></>
+              
+
+            ) : (
+              <label className="add" htmlFor="addid">
+                <input type="file" id="addid" onChange={handleFileChange} 
+                  
+                />
+                <FiPlus className="addfi" />
+              </label>
+            )}
           </div>
-          <div className="in-field">
-            <label className="inq">
-              Any Milestone Achieved During Childhood?
-            </label>
-            <input className="inp" type="text"></input>
-          </div>
-          <div className="in-field">
-            <label className="inq">
-              How Well Does the Individual Interact?
-            </label>
-            <input className="inp" type="text"></input>
-          </div>
-          <div className="in-field">
-            <label className="inq">
-              Any Repetitive Behaviour of the Individual?
-            </label>
-            <input className="inp" type="text"></input>
-          </div>
-          <div className="in-field">
-            <label className="inq">Any Other Concerns?</label>
-            <input className="inp" type="text"></input>
-          </div>
+          <p className="h2">UPLOAD <br></br>REPORT</p>
+
           <button
             className="submit"
             onClick={() => {
               localStorage.setItem("reported", "true");
-              alert("Report Updated Successfully");
-              document.getElementById("home-direct").click();
+              
+              if(fileUrl){    
+
+
+
+                alert("Your file has been successfully uploaded. We appreciate your submission");
+                document.getElementById("learn-direct").click();
+              }
+              else{
+                alert("Please upload a file before submitting the form");
+              }
             }}
           >
             Submit
           </button>
         </div>
       </div>
-      <Link to="/" id="home-direct"></Link>
+      
+      
+      <Link to="/MindMateCourseOfferings" id="learn-direct"></Link>
     </div>
   );
 };
